7fc24b4bb7c8d3fe39fdb3a5e9f7e0fdff09f25e
Dan Farnsworth <dan.farnsworth@evernym.com>
2020-05-29 17:13:42 -0600

    Reuse pool and wallet handles, separate selfserve logs from indy, clean up white space
    Signed-off-by: Dan Farnsworth <dan.farnsworth@evernym.com>

--- selfserve/main_selfserve/main.py
+++ selfserve/main_selfserve/main.py
@@ -5,6 +5,13 @@ from routes import setup_static_routes
 
 app = web.Application()
 app['pool_lock'] = asyncio.Lock()
+app['handles'] = {
+    'pools': {
+        'stagingnet': None,
+        'buildernet': None
+    },
+    'wallet': None
+}
 setup_routes(app)
 setup_static_routes(app)
-web.run_app(app, host='localhost', port=8080)
+web.run_app(app, host='127.0.0.1', port=8080)
