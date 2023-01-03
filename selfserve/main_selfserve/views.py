1d94f9635de03a728d73752442f8ff24001506ff
KoleBarnes <kbarnes261@gmail.com>
2023-01-03 14:32:05 -0800

    Redirect option to selfserve.
    Added:
     - redirect controlled with .env variables.
     - files to gitignore.
    
    Signed-off-by: KoleBarnes <kbarnes261@gmail.com>

--- selfserve/main_selfserve/views.py
+++ selfserve/main_selfserve/views.py
@@ -1,5 +1,14 @@
 from aiohttp import web
-
+import os
+from dotenv import load_dotenv
 
 async def index(request):
-    return web.FileResponse('./static/endorser.html')
+    load_dotenv()
+
+    REDIRECT = os.environ.get('REDIRECT')
+    REDIRECT_URL = os.environ.get('REDIRECT_URL')
+
+    if REDIRECT == 'True':
+        return web.HTTPTemporaryRedirect(REDIRECT_URL)
+    else:
+        return web.FileResponse('./static/endorser.html')
